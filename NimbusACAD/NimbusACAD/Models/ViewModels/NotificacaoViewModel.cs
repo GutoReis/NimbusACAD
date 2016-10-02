696a5e53da064dd77885c5e3383f9ba48a3dede1
Gustavo <guto.reisdelima@gmail.com>
2016-10-02 00:07:24 -0300

    Adição da classe NotificacaoViewModel

---
+++ NimbusACAD/NimbusACAD/Models/ViewModels/NotificacaoViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NimbusACAD.Models.ViewModels
+{
+    public class EscreverNotifacaoViewModel
+    {
+        [Required]
+        [Display(Name = "Para")]
+        public int ReceptorID { get; set; }
+
+        [Required]
+        [Display(Name = "Assunto")]
+        public string Assunto { get; set; }
+
+        [Required]
+        [Display(Name = "Menssagem")]
+        public string Corpo { get; set; }
+    }
+
+    public class LerNotifacaoViewModel
+    {
+        [Required]
+        [Display(Name = "De")]
+        public int EmissorID { get; set; }
+
+        [Required]
+        [Display(Name = "Assunto")]
+        public string Assunto { get; set; }
+
+        [Required]
+        [Display(Name = "Menssagem")]
+        public string Corpo { get; set; }
+    }
+
+    public class VerInfosNotifacaoViewModel
+    {
+        [Required]
+        [Display(Name = "De")]
+        public int EmissorID { get; set; }
+
+        [Required]
+        [Display(Name = "Para")]
+        public int ReceptorID { get; set; }
+
+        [Required]
+        [Display(Name = "Assunto")]
+        public string Assunto { get; set; }
+
+        [Required]
+        [Display(Name = "Menssagem")]
+        public string Corpo { get; set; }
+
+        [Required]
+        [Display(Name = "Lida")]
+        public bool Lida { get; set; }
+    }
+}
