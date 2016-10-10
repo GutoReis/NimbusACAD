d18723d3d58bbdc11d95586f7ec61de3c723e7f8
Gustavo <guto.reisdelima@gmail.com>
2016-10-10 11:51:29 -0300

    Remoção da camada model errada, pequenos ajustes na classe EmailManager.

--- NimbusACAD/NimbusACAD/Identity/Email/EmailManager.cs
+++ NimbusACAD/NimbusACAD/Identity/Email/EmailManager.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using NimbusACAD.Models;
-using System.Net.Mail;
+﻿using System.Net.Mail;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using System.Net.Security;
