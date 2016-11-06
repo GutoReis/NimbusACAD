fc746d6628513bfb5ea83342aba2d85b7e483472
Gustavo <guto.reisdelima@gmail.com>
2016-11-06 15:30:52 -0200

    Adição do controller Desautorizado.

---
+++ NimbusACAD/NimbusACAD/Controllers/DesautorizadoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NimbusACAD.Controllers
+{
+    public class DesautorizadoController : Controller
+    {
+        // GET: Desautorizado
+        public ActionResult Erro(string _errorMsg)
+        {
+            ViewBag.ErrorMsg = _errorMsg;
+            return View();
+        }
+    }
+}
